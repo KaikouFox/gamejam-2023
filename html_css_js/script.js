584b741b63a29c4e362289245c539a97d6fbc9e3
KaikouFox <cacauboon.vangember@gmail.com>
2023-07-14 11:19:59 +0200

    balk gemaakt met langzaam omlaag gaan

--- html_css_js/script.js
+++ html_css_js/script.js
@@ -1,4 +1,5 @@
 let player;
+let balk;
 let img;
 
 function preload() {
@@ -7,12 +8,15 @@ function preload() {
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  player = new Player(innerWidth/2-150, innerHeight/2-197, 300/1, 394/1, 10, 5, 5)
+  player = new Player(innerWidth/2-150, innerHeight/2-197, 300/4, 394/4, 10, 5, 5)
+  balk = new TimeBalk(innerWidth/16, 20, innerWidth-innerWidth/8, 50, 1, 0.1, 1.1)
+  frameRate(60)
 }
 
 function draw() {
   background(255)
   player.update()
+  balk.update()
 }
 
 class Player{
@@ -31,8 +35,8 @@ class Player{
   }
 
   update() {
-    this.move()
-    this.draw()
+    if (balk.w-balk.x/2 > balk.ss) {this.move();}
+    this.draw();
   }
 
   move() {
@@ -50,6 +54,41 @@ class Player{
 
     if (keyIsDown(83)) {
       this.y += this.sy;
+    }
+  }
+}
+
+class TimeBalk {
+  constructor(x, y, w, h, ss, s, a) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.ss = ss;
+    this.s = s;
+    this.a = a;
+  }
+
+  update() {
+    this.draw()
+    if (this.s != 0) {this.changeSpeed()}
+    this.checkForDeath()
+  }
+
+  draw() {
+    fill(0)
+    rect(this.x, this.y, this.w, this.h)
+    fill(255,0,0)
+    rect(this.x/4 + this.x, this.y+this.y/2, this.w-this.x/2-this.ss, this.h-this.y)
+  }
+
+  changeSpeed() {
+    this.ss = this.ss + this.s
+  }
+
+  checkForDeath() {
+    if (this.w-this.x/2 <= this.ss) {
+      this.ss = this.w-this.x/2
     }
   }
 }
